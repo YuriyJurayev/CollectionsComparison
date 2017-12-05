822b91ff392c87a9c54fc71b6f4f8254c663d02a
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-12-05 11:48:44 +0600

    reused new methods

--- src/main/java/kz/epam/tam/module3/lecture234/collectionscomparison/measurements/PerformanceComparator.java
+++ src/main/java/kz/epam/tam/module3/lecture234/collectionscomparison/measurements/PerformanceComparator.java
@@ -1,53 +1,58 @@
 package kz.epam.tam.module3.lecture234.collectionscomparison.measurements;
 
+import kz.epam.tam.module3.lecture234.collectionscomparison.core.AbstractMethods;
 import kz.epam.tam.module3.lecture234.collectionscomparison.core.ListMethods;
+import kz.epam.tam.module3.lecture234.collectionscomparison.core.MapMethods;
+import kz.epam.tam.module3.lecture234.collectionscomparison.core.SetMethods;
 import kz.epam.tam.module3.lecture234.collectionscomparison.utils.PerformancePrinter;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class PerformanceComparator {
-
-    private PerformanceMeasurer measurer = new PerformanceMeasurer();
 
     public void compareListsPerformance(){
         PerformancePrinter.printStartMsg();
         PerformancePrinter.printType("List's");
         PerformancePrinter.printAction("Adding","to","list");
-
-
-        measurer.measure(1,1,"Addition","ArrayList");
-        measurer.measure(1,1,"Addition","LinkedList");
+        AbstractMethods<List<Integer>> list = new ListMethods();
+        ListMethods methodL = new ListMethods();
+        list.addition(methodL.createArrayList(), "Addition","ArrayList");
+        list.addition(methodL.createLinkedList(), "Addition","LinkedList");
         PerformancePrinter.printAction("Searching","in","list");
-        measurer.measure(1,2,"Search","ArrayList");
-        measurer.measure(1,2,"Search","LinkedList");
+        list.search(methodL.createArrayList(), "Search","ArrayList");
+        list.search(methodL.createLinkedList(), "Search","LinkedList");
         PerformancePrinter.printAction("Deleting","from","list");
-        measurer.measure(1,3,"Deletion","ArrayList");
-        measurer.measure(1,3,"Deletion","LinkedList");
+        list.deletion(methodL.createArrayList(), "Deletion","ArrayList");
+        list.deletion(methodL.createLinkedList(), "Deletion","ArrayList");
     }
     public void compareSetsPerformance(){
-
         PerformancePrinter.printType("Set's");
         PerformancePrinter.printAction("Adding","to","set");
-        measurer.measure(2,1,"Addition","HashSet");
-        measurer.measure(2,2,"Addition","TreeSet");
+        AbstractMethods<Set<String>> set = new SetMethods();
+        SetMethods methodS = new SetMethods();
+        set.addition(methodS.createHashSet(), "Addition","HashSet");
+        set.addition(methodS.createTreeSet(), "Addition","TreeSet");
         PerformancePrinter.printAction("Searching","in","set");
-        measurer.measure(2,3,"Search","HashSet");
-        measurer.measure(2,4,"Search","TreeSet");
+        set.search(methodS.createHashSet(), "Search","HashSet");
+        set.search(methodS.createTreeSet(), "Search","TreeSet");
         PerformancePrinter.printAction("Deleting","from","set");
-        measurer.measure(2,5,"Deletion","HashSet");
-        measurer.measure(2,6,"Deletion","TreeSet");
-
+        set.deletion(methodS.createHashSet(), "Deletion","HashSet");
+        set.deletion(methodS.createTreeSet(), "Deletion","TreeSet");
     }
     public void compareMapsPerformance(){
         PerformancePrinter.printType("Map's");
         PerformancePrinter.printAction("Adding","to","map");
-        measurer.measure(3,1,"Addition","HashMap");
-        measurer.measure(3,2,"Addition","TreeMap");
+        AbstractMethods<Map<Integer,String>> map = new MapMethods();
+        MapMethods methodM = new MapMethods();
+        map.addition(methodM.createHashMap(),"Addition","HashMap");
+        map.addition(methodM.createTreeMap(),"Addition","TreeMap");
         PerformancePrinter.printAction("Searching","in","map");
-        measurer.measure(3,3,"Search","HashMap");
-        measurer.measure(3,4,"Search","TreeMap");
+        map.search(methodM.createHashMap(),"Search","HashMap");
+        map.search(methodM.createTreeMap(),"Search","TreeMap");
         PerformancePrinter.printAction("Deleting","from","map");
-        measurer.measure(3,5,"Deletion","HashMap");
-        measurer.measure(3,6,"Deletion","TreeMap");
+        map.deletion(methodM.createHashMap(),"Deletion","HashMap");
+        map.deletion(methodM.createTreeMap(),"Deletion","TreeMap");
     }
 }
