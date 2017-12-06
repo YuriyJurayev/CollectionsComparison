babd08fbed7f145da257ada62d35254ebe6a6a7d
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-12-06 14:14:35 +0600

    added start msg method call

--- src/main/java/kz/epam/tam/module3/lecture234/collectionscomparison/runner/Runner.java
+++ src/main/java/kz/epam/tam/module3/lecture234/collectionscomparison/runner/Runner.java
@@ -1,10 +1,12 @@
 package kz.epam.tam.module3.lecture234.collectionscomparison.runner;
 
 import kz.epam.tam.module3.lecture234.collectionscomparison.measurements.PerformanceComparator;
+import kz.epam.tam.module3.lecture234.collectionscomparison.utils.PerformancePrinter;
 
 public class Runner {
     public static void main(String[] args){
 
+        PerformancePrinter.printStartMsg();
         PerformanceComparator comparator = new PerformanceComparator();
         comparator.compareListsPerformance();
         comparator.compareSetsPerformance();
